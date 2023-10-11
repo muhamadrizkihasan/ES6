a95beda5893778e07561547504f95d3a7bb74ba7
Muhamad Rizki Hasan <muhamadrizkihasan@smkwikrama.sch.id>
2023-10-11 12:43:03 +0700

    Menambahkan folder execution context, hoisting & scope

---
+++ 2.ExecutionContext,Hoisting&Scope/script.js
@@ -0,0 +1,11 @@
+// 2.1 EXCEPTION Context, HOISTING & 
+
+var nama = 'Hasan';
+console.log(nama);
+
+// creation phase pada Global Context
+// nama var = undefined
+// nama function = fn()
+// hoisting
+// window = global object
+// this = window
