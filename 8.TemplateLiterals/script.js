8a668d96ba5d9fa478fe84ad124feb45adac2608
unknown <muhamadrizkihasan@smkwikrama.sch.id>
2023-12-01 10:53:03 +0700

    Menambahkan folder template literals

---
+++ 8.TemplateLiterals/script.js
@@ -0,0 +1,8 @@
+// Template literals / Template String
+const nama = 'Hasan';
+const umur = 33;
+console.log(`Halo, nama saya ${nama}, dan saya ${umur} tahun`);
+
+// Embedded Expressions
+const x = 11;
+console.log(`${(x % 2 == 0) ? 'genap' : 'ganjil'}`);
